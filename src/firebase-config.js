2e0e5849c34e1ee181680ffb551eb94a76d39de2
kuhuhalder <kuhuhalder2701@gmail.com>
2023-01-12 21:07:56 -0500

    first commit

---
+++ src/firebase-config.js
@@ -0,0 +1,53 @@
+/**
+ * To find your Firebase config object:
+ * 
+ * 1. Go to your [Project settings in the Firebase console](https://console.firebase.google.com/project/_/settings/general/)
+ * 2. In the "Your apps" card, select the nickname of the app for which you need a config object.
+ * 3. Select Config from the Firebase SDK snippet pane.
+ * 4. Copy the config object snippet, then add it here.
+ */
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDm3maXz7jC_fCe7ysAMhXXYS4_MMnIPmU",
+  authDomain: "friendlychat-407aa.firebaseapp.com",
+  projectId: "friendlychat-407aa",
+  storageBucket: "friendlychat-407aa.appspot.com",
+  messagingSenderId: "883250162931",
+  appId: "1:883250162931:web:cf18a5580574f4b5f7db86",
+  measurementId: "G-P935KJZR3J"
+};
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+const config = {
+  /* TODO: ADD YOUR FIREBASE CONFIGURATION OBJECT HERE */
+  apiKey: "AIzaSyDm3maXz7jC_fCe7ysAMhXXYS4_MMnIPmU",
+  authDomain: "friendlychat-407aa.firebaseapp.com",
+  projectId: "friendlychat-407aa",
+  storageBucket: "friendlychat-407aa.appspot.com",
+  messagingSenderId: "883250162931",
+  appId: "1:883250162931:web:cf18a5580574f4b5f7db86",
+  measurementId: "G-P935KJZR3J"
+};
+
+
+
+export function getFirebaseConfig() {
+  if (!config || !config.apiKey) {
+    throw new Error('No Firebase configuration object provided.' + '\n' +
+    'Add your web app\'s configuration object to firebase-config.js');
+  } else {
+    return config;
+  }
+}
+
